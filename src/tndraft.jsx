6f41899a444a5bf6d9786bcd4b447212ec08b01f
Leandro Almeida <leandro_almeida@artear.com.ar>
2016-07-11 10:19:30 -0300

    use richtexteditor

--- src/tndraft.jsx
+++ src/tndraft.jsx
@@ -1,5 +1,5 @@
-import Editor from './editors/MyMegadraft.jsx';
-//import Editor from './editors/RichTextEditor.jsx';
+//import Editor from './editors/MyMegadraft.jsx';
+import Editor from './editors/RichTextEditor.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
